867ccc2d160eb5e8f662516f4f6a6ad5e0c9a031
Arthur Correnson <arthur.correnson@gmail.com>
2020-03-22 17:39:46 +0100

    Update language with Axiom declaration and multiple proof script

--- lib/kernel/checker.mli
+++ lib/kernel/checker.mli
@@ -2,4 +2,12 @@ open Term
 
 type type_env
 val new_env : type_env
+
+type error =
+  | Bad_type of ptype
+  | Not_typable
+  | Overwrite
+
 val type_check : term -> type_env -> ptype option
+val extend_env : string -> ptype -> term -> type_env -> (type_env, error) result
+val extend_by_axiom : string -> ptype -> type_env -> (type_env, error) result
--- lib/language/parser.ml
+++ lib/language/parser.ml
@@ -2,9 +2,11 @@ open Kernel.Term
 open Parsing
 
 type entity =
-  | Axiom of ptype
-  | Target of ptype * term
+  | Axiom of string * ptype
+  | Target of (string * ptype) * term
 [@@deriving variants]
+
+let named_prop a b = (a, b)
 
 let parse_type =
   let rec atom_type inp =
@@ -19,7 +21,8 @@ let parse_type =
 
 
 let parse_prop_stmt =
-  literal "Prop" *> spaced (char ':') *> parse_type
+  named_prop <$> literal "Prop" *> (spaced stringlitl) <*> spaced (char ':') *> parse_type
+  |> trimed
 
 let parse_bind = bind <$> stringlitl <*> spaced (char ':') *> parse_type
 
@@ -47,9 +50,14 @@ let parse_term =
 
 let parse_proof_stmt =
   literal "Proof" *> spaced (char ':') *> parse_term
+  |> trimed
+
+let parse_axiom =
+  axiom <$> literal "Axiom" *> (spaced stringlitl) <*> spaced (char ':') *> parse_type
+  |> trimed
 
 let parse_script =
-  many (target <$> parse_prop_stmt <*> blanks *> parse_proof_stmt)
+  many ((target <$> parse_prop_stmt <*> blanks *> parse_proof_stmt) <|> parse_axiom)
 
 
 let read_all f =
@@ -59,51 +67,3 @@ let read_all f =
     with End_of_file -> acc
   in
   loop ""
-
-(* open Kernel
-   open Term
-   open Parsing
-   open Sexplib
-
-   let rec parse_atom_type inp =
-   inp --> (t_atom <$> stringlitu <|> parenthesized '{' ~~parse_type '}')
-
-   and parse_type inp =
-   inp
-   --> ( t_arrow <$> ~~parse_atom_type <*> arrow *> ~~parse_type
-        <|> ~~parse_atom_type )
-
-   and parse_annotation inp =
-   inp --> (bind <$> stringlitl <*> (String.make 1 <$> char ':') *> ~~parse_type)
-
-   let rec parse_term s =
-   let open Sexp in
-   match s with
-   | Atom s -> Var s
-   | List [ Atom "lambda"; List [ Atom binding ]; expr ] ->
-    Lam (parse_binding binding, parse_term expr)
-   | List [ a; b ] -> App (parse_term a, parse_term b)
-   | _ -> failwith ("parsing error " ^ to_string_hum s)
-
-   and parse_binding b =
-   match do_parse ~~parse_annotation b with
-   | Some x -> x
-   | None -> failwith ("parsing error " ^ b)
-
-   let parse_prop p =
-   let open Sexp in
-   match p with
-   | Atom s -> (
-      match do_parse ~~parse_type s with
-      | Some t -> t
-      | _ -> failwith ("invalid proposition " ^ s) )
-   | _ -> failwith "invalid syntax : missing valid 'Prop' clause"
-
-   let parse_script s =
-   let open Sexp in
-   match s with
-   | List [ List [ Atom "Prop"; p ]; List [ Atom "Proof"; q ] ] ->
-    let prop = parse_prop p in
-    let proof = parse_term q in
-    (prop, proof)
-   | _ -> failwith "Syntax error in the script" *)
